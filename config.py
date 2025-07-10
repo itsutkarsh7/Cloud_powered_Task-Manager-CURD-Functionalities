ef980c955a29abb7493c4568bcbd4a3c34ca895c
Utkarsh Singh <your-email@example.com>
2025-07-10 15:00:18 +0530

    Fix import case error

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-Cloudant_config = {
+cloudant_config = {
     "username": os.getenv("CLOUDANT_USERNAME"),
     "apikey": os.getenv("CLOUDANT_APIKEY"),
     "url": "https://<your-cloudant-username>.cloudantnosqldb.appdomain.cloud",  # Replace with your actual Cloudant URL
